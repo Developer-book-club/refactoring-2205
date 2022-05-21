ee2883e02119f5677e942e4f996d1075ce5fbc87
ljh95 <ilegwmam@gmail.com>
2022-05-22 07:11:44 +0900

    중간점검: 두 파일(과 두 단계)로 분리됨

--- jhlee/2week/app.js
+++ jhlee/2week/app.js
@@ -1,78 +1,35 @@
-import { invoice } from './invoices.js';
-import { plays } from './plays.js';
+import { createStatementData } from './createStatementData.js';
 
 export function statement(invoice, plays) {
-  let result = `청구 내역(고객명: ${invoice.customer})\n`;
+  return renderPlainText(createStatementData(invoice, plays));
 
-  for (let perf of invoice.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-  }
+  function renderPlainText(data) {
+    let result = `청구 내역(고객명: ${data.customer})\n`;
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
+    for (let perf of data.performances) {
+      result += ` ${perf.play.name}: ${usd(perf.amount)} (${
+        perf.audience
+      }석)\n`;
     }
-    return result;
-  }
 
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += volumeCreditsFor(perf);
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
+
+    function usd(aNumber) {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      }).format(aNumber / 100);
     }
-    return result;
   }
 
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
+  function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
 
-      default:
-        throw new Error(`알 수 없는 장릐: ${playFor(aPerformance).type}`);
+    function renderHtml(data) {
+      let result = ``;
     }
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-
-    if ('comedy' === playFor(aPerformance).type)
-      volumeCredits += Math.floor(aPerformance.audience / 5);
-
-    return volumeCredits;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
   }
 }
-console.log(statement(invoice, plays));
