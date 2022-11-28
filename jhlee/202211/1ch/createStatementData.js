e3f31dddc426497f233e0bd6a180bb580fd01353
ljh95 <ilegwmam@gmail.com>
2022-11-29 00:38:34 +0900

    refact: make 다형성

--- jhlee/202211/1ch/createStatementData.js
+++ jhlee/202211/1ch/createStatementData.js
@@ -51,18 +51,9 @@ class PerformanceCalculator {
 
     switch (this.play.type) {
       case 'tragedy': // 비극
-        result = 40000;
-        if (this.performances.audience > 30) {
-          result += 1000 * (this.performances.audience - 30);
-        }
-        break;
+        throw '오류 발생';
       case 'comedy':
-        result = 30000;
-        if (this.performances.audience > 20) {
-          result += 10000 + 500 * (this.performances.audience - 20);
-        }
-        result += 300 * this.performances.audience;
-        break;
+        throw '오류 발생';
       default:
         throw new Error(`알 수 없는 장르: ${this.play.type}`);
     }
@@ -79,5 +70,26 @@ class PerformanceCalculator {
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
-class ComedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+
+    if (this.performances.audience > 30) {
+      result += 1000 * (this.performances.audience - 30);
+    }
+
+    return result;
+  }
+}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+
+    if (this.performances.audience > 20) {
+      result += 10000 + 500 * (this.performances.audience - 20);
+    }
+    result += 300 * this.performances.audience;
+
+    return result;
+  }
+}
