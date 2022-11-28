81fdc943baa4e6810f73278fa5206e6851e059db
ljh95 <ilegwmam@gmail.com>
2022-11-28 23:53:54 +0900

    feat: create renderHtml

--- jhlee/202211/1ch/1-3.js
+++ jhlee/202211/1ch/1-3.js
@@ -16,6 +16,23 @@ export function statement(invoice, plays) {
     return result;
   }
 
+  function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+  }
+
+  function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+
+    for (let perf of data.performances) {
+      result += `<p> ${perf.play.name}: ${usd(perf.amount)} (${
+        perf.audience
+      }석)</p>\n`;
+    }
+    result += `<p>총액: ${usd(data.totalAmount)}</p>\n`;
+    result += `<p>적립 포인트: ${data.totalVolumCredits}점</p>\n`;
+    return result;
+  }
+
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
