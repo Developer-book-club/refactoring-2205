e06ae361b0801925da3114a8143c50c1779b7c77
ljh95 <ilegwmam@gmail.com>
2022-11-28 23:51:18 +0900

    refact: divide file creatStatementData

--- jhlee/202211/1ch/1-3.js
+++ jhlee/202211/1ch/1-3.js
@@ -1,49 +1,7 @@
+import { createStatementData } from './createStatementData.js';
+
 export function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice), plays);
-
-  function createStatementData(invoice) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalVolumCredits = totalVolumCredits(statementData);
-    statementData.totalAmount = totalAmount(statementData);
-    return statementData;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
+  return renderPlainText(createStatementData(invoice, plays), plays);
 
   function renderPlainText(data) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
@@ -58,28 +16,11 @@ export function statement(invoice, plays) {
     return result;
   }
 
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
-
-  function totalVolumCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
   }
 }
