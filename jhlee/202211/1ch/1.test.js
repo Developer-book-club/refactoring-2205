efece74b2670c35e77d4c5d47fc558a195fdbdf2
ljh95 <ilegwmam@gmail.com>
2022-11-28 22:46:44 +0900

    feat: 1-2

---
+++ jhlee/202211/1ch/1.test.js
@@ -0,0 +1,16 @@
+import plays from './plays.json' assert { type: 'json' };
+import invoices from './invoices.json' assert { type: 'json' };
+import { statement } from './1-2.js';
+
+// console.log(plays);
+// console.log(invoices);
+const s = `청구 내역 (고객명: BigCo)
+ Hamlet: $650.00 (55석)
+ As You Like It: $580.00 (35석)
+ Othello: $500.00 (40석)
+총액: $1,730.00
+적립 포인트: 47점
+`;
+test('defaultTest', () => {
+  expect(statement(invoices[0], plays)).toBe(s);
+});
